fb51807aa15fa22e995bd02cd2f42675b0c12995
brotalnia <brotalnia@gmail.com>
2017-05-24 17:17:23 +0300

    text

--- sql/migrations/20170519115936_world.sql
+++ sql/migrations/20170519115936_world.sql
@@ -132,7 +132,7 @@ UPDATE `creature_template` SET `gossip_menu_id`=4512, `ScriptName`='' WHERE `ent
 
 -- Frahun Shadewhisper
 DELETE FROM `npc_gossip` WHERE `npc_guid`=46179;
-INSERT INTO `gossip_menu` VALUES (4690, 4794, 100);
+INSERT INTO `gossip_menu` VALUES (4690, 4795, 100);
 INSERT INTO `gossip_menu` VALUES (4690, 4796, 101);
 INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `condition_id`) VALUES (4690, 0, 3, 'I seek training.', 5, 16, 0, 0, 0, 0, 0, '', 0);
 INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `condition_id`) VALUES (4690, 1, 0, 'I wish to unlearn my talents.', 1, 1, 4461, 0, 0, 0, 0, '', 136);
