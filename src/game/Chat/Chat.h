29020258113ecec4f7a3c9035c5777d5e2aa3828
brotalnia <brotalnia@gmail.com>
2017-08-05 19:49:20 +0300

    Implement Quest Greetings.

--- src/game/Chat/Chat.h
+++ src/game/Chat/Chat.h
@@ -682,6 +682,7 @@ class MANGOS_DLL_SPEC ChatHandler
         bool HandleReloadQuestEndScriptsCommand(char* args);
         bool HandleReloadQuestStartScriptsCommand(char* args);
         bool HandleReloadQuestTemplateCommand(char* args);
+        bool HandleReloadQuestGreetingCommand(char* args);
         bool HandleReloadReservedNameCommand(char* args);
         bool HandleReloadReputationRewardRateCommand(char* args);
         bool HandleReloadReputationSpilloverTemplateCommand(char* args);
